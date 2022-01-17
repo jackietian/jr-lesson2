8ee06b74fcec246b4def0fb764ca613b47b3d9ff
Jackie Tian <>
2022-01-18 10:12:13 +1100

    add scope comments

--- src/scope/scope.js
+++ src/scope/scope.js
@@ -1,33 +1,19 @@
-
-// scope: block and functional scope
-
-// block scope: const or let
-// funcational scope: var
-
 /**
  * let is block scope
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
  */
 
-//  var x = 1;
-// var x
-// console.log(x)
+// let x = 1;
 
 // if (x === 1) {
-//     x = 2;
+//   let x = 2;
 
-//     console.log(x);
-//     // expected output: 2
-// } else {
-//     x = 3
-//     console.log(x)
+//   console.log(x);
+//   // expected output: 2
 // }
 
-//  x = 1;
- 
-//  console.log(x);
- // expected output: 1
-
+// console.log(x);
+// // expected output: 1
 
 /**
  * const is block scope as well
@@ -48,28 +34,30 @@
 /**
  * var is functional scope
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
- * 
+ *
  * hoisting
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var#var_hoisting
- * 
+ *
  * declaration will be hoisted, not initialization
+ *
+ * Variables declared with let and const are also hoisted but, unlike var, are not initialized with a default value.
+ * An exception will be thrown if a variable declared with let or const is read before it is initialized
  */
 
-// var x = 1;
+var x = 1;
 
-// if (x === 1) {
-//   var x = 2;
+if (x === 1) {
+  var x = 2;
 
-//   console.log(x); // 2
-// }
+  console.log(x); // 2
+}
 
-// console.log(x); // 2
+console.log(x); // 2
 
-// An IIFE (Immediately Invoked Function Expression) 
+// An IIFE (Immediately Invoked Function Expression)
 // (function () {
-//     console.log('-------')
+//   console.log("-------");
 // })();
-
 
 /**
  * =======================================
@@ -92,46 +80,46 @@
  * closure
  */
 // function createCounter(a) {
-//     let number = 0;
+//   let number = 0;
 
-//     // a,b,c...
-//     return function counter(b) {
-        
-//         number++;
+//   // a,b,c...
+//   return function counter(b) {
+//     number++;
 
-//         // has access to a, b , c
-//         return number + a + b;
-//     }
+//     // has access to a, b , c
+//     return number + a + b;
+//   };
 // }
 
-// const response = createCounter('a')
-// console.log(response)
-// console.log(response('b'))
+// const response = createCounter("a");
+// console.log(response);
+// console.log(response("b"));
 
 function makeFuncs() {
-    const funcs = [];
-    for (let i = 0; i < 3; i++) {
-        funcs[i] = function() {
-            console.log(`func ${i}: ${i}`);
-        };
-    }
-    // console.log(i)
-    // i = 3 when var
-    // i is not defined when let
-    return funcs;
+  const funcs = [];
+  for (var i = 0; i < 3; i++) {
+    console.log("inside block scope of for loop", i);
+    funcs[i] = function () {
+      console.log(`func ${i}: ${i}`);
+    };
+  }
+  // console.log(i);
+  // i = 3 when var, and i is the same outside for loop and inside for loop
+  // i is not defined when let, and there are 2 variables(one inside the for loop, the other is outside the for loop)
+  return funcs;
 }
 const functions = makeFuncs();
 
 /**
  * functions = [
-    * function() { console.log(`func ${i}: ${i}`)},
-    * function() { console.log(`func ${i}: ${i}`)},
-    * function() { console.log(`func ${i}: ${i}`)},
+ * function() { console.log(`func ${i}: ${i}`)},
+ * function() { console.log(`func ${i}: ${i}`)},
+ * function() { console.log(`func ${i}: ${i}`)},
  * ]
  */
 
 for (var j = 0; j < 3; j++) {
-    functions[j]();
+  functions[j]();
 }
 
 /**
@@ -140,32 +128,30 @@ for (var j = 0; j < 3; j++) {
  */
 
 function makePistol() {
-    let bullets = 6;
-    return {
-        log: function() {
-            console.log(bullets)
-        },
-        shoot: function() {
-            if (bullets === 0) {
-                console.log('No bullets left!');
-                return;
-            }
-            bullets--;
-            console.log(`Bang! Bullets left: ${bullets}`);
-        },
-        reload: function() {
-            bullets = 6;
-            console.log(`Reloaded! Bullets left: ${bullets}`);
-        }
-    }
+  let bullets = 6;
+  return {
+    log: function () {
+      console.log(bullets);
+    },
+    shoot: function () {
+      if (bullets === 0) {
+        console.log("No bullets left!");
+        return;
+      }
+      bullets--;
+      console.log(`Bang! Bullets left: ${bullets}`);
+    },
+    reload: function () {
+      bullets = 6;
+      console.log(`Reloaded! Bullets left: ${bullets}`);
+    },
+  };
 }
-
 
 // const variable = 1, '1', true, fucntion
 // const variable1 = {}
 // variable1.name = 'asdf'
 // variable1.age = 'asdf'
-
 
 // // console.log(makePistol())
 // const pistol1 = makePistol()
